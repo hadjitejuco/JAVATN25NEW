ec5fa28a742788e20cd2fe63733bc0a7273b51bd
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 17:16:23 +0800

    first commit

---
+++ src/Switch1.java
@@ -0,0 +1,49 @@
+
+public class Switch1 {
+    public static void main(String[] args) {
+        /*
+            switch(condition) {
+                case 1:
+                    statement;
+                    break;
+                case 2:
+                    statement;
+                    break;
+                case 3:
+                    statement;
+                    break;
+                default:
+                    statement;
+        }
+        */
+        int day = 1;
+        switch (day){
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+                default:
+                    System.out.println("Invalid date");
+    
+    }
+        
+        
+    }
+}
