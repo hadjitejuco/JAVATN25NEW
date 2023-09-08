f8ef24372a59e0d7aabd8544ee06aacec0701e92
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 14:25:41 +0800

    first commit

---
+++ src/If2.java
@@ -0,0 +1,11 @@
+
+public class If2 {
+    public static void main(String[] args) {
+        String name = "Ivana";
+        
+        //if statement (single) 
+        if (name == "Ivana"){
+            System.out.println("Hello World");
+        }
+    }
+}
