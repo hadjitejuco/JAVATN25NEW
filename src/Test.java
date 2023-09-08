e9b4ec8b1cf994c81887f7fc0c7a945adbce86ae
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 13:37:46 +0800

    first commit

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,17 @@
-
+//https://github.com/hadjitejuco/JAVATN25NEW
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World again");
+        //division
+//        System.out.println(9/2);
+//        System.out.println(9.0/2);
+//        System.out.println(9/2.0);
+//        System.out.println(9.0/2.0);
+        
+        //assignment operators
+        // =
+        
+        
+        
+        
     }
 }
