33a8faae4905f4bc93f99c1d609a69b376586510
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 14:23:22 +0800

    first commit

---
+++ src/If1.java
@@ -0,0 +1,30 @@
+
+//If statement
+public class If1 {
+    public static void main(String[] args) {
+        /*
+        Single / Simple 
+        If statement
+        
+        Syntax 
+        if (condition){
+            code
+        }
+        */
+        
+        //determine if a number is pos or negative
+        //number is > 0 -> positive
+        //number is < 0 -> negative
+        
+        int x = 10;
+        int y = -1;
+        if (x>0){
+            System.out.println("Number is positive");
+        }
+        if (y < 0){
+            System.out.println("Number is negative");
+        }
+        
+        
+    }
+}
