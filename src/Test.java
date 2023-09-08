dbeb0106834991e2dd834206d8ff710c21697952
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-08 13:54:46 +0800

    first commit

--- src/Test.java
+++ src/Test.java
@@ -9,9 +9,41 @@ public class Test {
         
         //assignment operators
         // =
+//        //create a variable
+//        int x  = 4;
+//        int y;
+//        
+//        //assign value using the =
+//        y = x;
+//        System.out.println("Value of y is "+y);
+//        
+//        //+=, -=, *=, /+, %=
+//        y += x; // y = x + x
+//        System.out.println("value using += "+y);
+//        y *= x;
+//        System.out.println("value using *= "+y);
         
-        
-        
-        
+          //logical operators
+          /*
+            AND && OR || NOT !
+                            AND         OR          NOT
+                            &&          ||          !
+                            FAND        TOR
+          t     t           t           t       t   f
+          t     f           f           t       f   t
+          f     t           f           t
+          f     f           f           f
+          */
+          System.out.println((5>3)&&(8>5));
+          System.out.println((5>3)&&(8<5));
+          System.out.println((5<3)&&(8>5));
+          System.out.println((5<3)&&(8<5));
+          System.out.println((5>3)||(8>5));
+          System.out.println((5>3)||(8<5));
+          System.out.println((5<3)||(8>5));
+          System.out.println((5<3)||(8<5));
+          System.out.println(!(5==3));
+          System.out.println(!(5>3));
+
     }
 }
