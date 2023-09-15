b323183a15f2347e5ba16c46211f937996d6ab78
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-15 14:14:39 +0800

    first commit

--- src/For3.java
+++ src/For3.java
@@ -1,12 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author hadjitejuco
- */
 public class For3 {
-    
+    public static void main(String[] args) {
+        //break and continue
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" " + i);
+            if (i==5)
+                break;
+        }
+        System.out.println("Loop stopped");
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(" " + i);
+        }
+        
+    }
 }
