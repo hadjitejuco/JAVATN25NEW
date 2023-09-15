94f69a575b138e1402abdaf6cb31ab80f7477188
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-09-15 14:09:08 +0800

    first commit

---
+++ src/For2.java
@@ -0,0 +1,12 @@
+//compute for the sum of the series of number
+//compute for sum of all even and odd numbers
+public class For2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum = sum + i;
+        }
+        System.out.println("The sum is "+sum);
+    }
+ 
+}
