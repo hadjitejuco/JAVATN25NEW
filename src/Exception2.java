a15028dc21e4585b9e1151d39ae84506c4d21876
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-10-20 13:56:12 +0800

    first commit

---
+++ src/Exception2.java
@@ -0,0 +1,16 @@
+
+public class Exception2 {
+    
+    static void subroutine(){
+        int d =  0;
+        int a = 10/d;
+    
+    }
+    public static void main(String[] args) {
+        Exception2.subroutine();
+    }
+
+
+
+    
+}
