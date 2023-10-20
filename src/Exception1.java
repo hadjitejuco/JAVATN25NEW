8f7b5449262279aba3e81d8476992b197842674c
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-10-20 13:41:31 +0800

    first commit

---
+++ src/Exception1.java
@@ -0,0 +1,9 @@
+
+public class Exception1 {
+    public static void main(String[] args) {
+        int d = 0;
+        int a = 10/d;
+        System.out.println(a);
+        //JVM handle the errors found 
+    }
+}
