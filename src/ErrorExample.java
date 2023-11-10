c1886f61bea2777cdf96f97cc4df42f4b9b4dc5a
Hadji Tejuco <hadji.tejuco@gmail.com>
2023-11-10 14:10:49 +0800

    first commit

--- src/ErrorExample.java
+++ src/ErrorExample.java
@@ -1,12 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author hadjitejuco
- */
+
+//system.err (standard error stream)
+
+import java.util.Scanner;
+
 public class ErrorExample {
-    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println("You entered "+number);
+        
+        }catch(Exception e) {
+            System.err.println("Error: Invalid Input");
+        
+        }finally {
+        
+        }
+        
+    }
 }
